17c2f334f32edc4acfedfc31d54f130f4718385a
Ranuga-D <79456372+Programmer-RD-AI@users.noreply.github.com>
2021-09-07 23:23:11 +0530

    Add files via upload

--- main1.sql
+++ main1.sql
@@ -9,3 +9,12 @@ CREATE TABLE employee (
     branch_id INT,
 )
 
+CREATE TABLE branch (
+    branch_id INT PRIMARY KEY,
+    branch_name VARCHAR(40),
+    mgr_id INT,
+    mgr_start_date DATE,
+    FOREIGN KEY (mgr_id) REFERENCES employee(emp_id) ON DELETE SET NULL
+)
+
+ALTER TABLE employee ADD FOREIGN KEY (branch_id) REFERENCES branch
