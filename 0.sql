8637c03026c8393abfc7ecf0ba76bb982a98987e
Ranuga-D <79456372+Programmer-RD-AI@users.noreply.github.com>
2021-09-08 00:01:06 +0530

    Add files via upload

---
+++ 0.sql
@@ -0,0 +1,20 @@
+-- SELECT
+--     1 + 1 AS Result,
+--     3 + 3 AS Result2,
+--     4 * 4 AS [Result 3] 
+-- FROM sys.objects
+-- SELECT
+--     [name],[object_id],[type_desc]
+-- FROM
+--     sys.objects
+
+
+-- SELECT
+--     [name] AS NameOfObject,[object_id],[type_desc]
+-- FROM
+--     sys.objects
+
+
+SELECT 11*12 AS [My Answer]
+
+SELECT [object_id] AS ObjectID,[name] AS ObjectName,[column_id] AS ColumnID FROM sys.columns
